7f2e34f2cdfbaaa0e65b38727803cbcb2932d515
DanutPopa <popa.ion.danut@gmail.com>
2024-12-04 22:45:54 +0200

    Designing a single card view.

---
+++ Flashzilla/Card.swift
@@ -0,0 +1,15 @@
+//
+//  Card.swift
+//  Flashzilla
+//
+//  Created by Danut Popa on 04.12.2024.
+//
+
+import Foundation
+
+struct Card {
+    var prompt: String
+    var answer: String
+    
+    static let example = Card(prompt: "Who played the 13th Doctor in Doctor Who?", answer: "Jodie Whittaker")
+}
